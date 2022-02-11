7feedc99b77642a42f0b42f9c9b8e47436cd58b1
finnbett <85616388+finnbett@users.noreply.github.com>
2022-02-11 14:45:02 +1300

    made the score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from score import Score
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.tracer(0)
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Score()
 
 
 screen.listen()
@@ -22,19 +24,30 @@ screen.onkey(left_paddle.move_paddle_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    game_speed = ball.game_speed
+    time.sleep(game_speed)
     screen.update()
     ball.ball_move()
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
+
     #detect collision with the right paddle
     if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) <50 and ball.xcor() < -320:
         ball.bounce_x()
 
+
     #detecct if ball is out of bounds
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380:
         ball.reset()
+        score.left_point()
+        score.update_score()
+
+    if ball.xcor() < -380:
+        ball.reset()
+        score.right_point()
+        score.update_score()
+
 
 
 
