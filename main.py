43a445a9900818c8a81e0fffcad2be228a71c3d6
finnbett <85616388+finnbett@users.noreply.github.com>
2022-02-11 13:37:42 +1300

    made the ball

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.bgcolor("black")
 screen.tracer(0)
-#TODO: Create and move the paddle
+
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -19,11 +22,13 @@ screen.onkey(left_paddle.move_paddle_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.ball_move()
 
 
 screen.exitonclick()
-#TODO: CREATE ANOTHER PADDLE
+
 #TODO: CREATE THE BALL AND MAKE IT MOVE
 #TODO: DETECT COLLIISON WITH WALL AND BOUNCE
 #TODO: DETECT COLLISION WITH THE PADDLE AND BOUNCE
